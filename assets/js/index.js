873af68310be59105f190f7f9d0196f8ac5e5a03
mattzahel <mateusz.zahel@gmail.com>
2020-02-01 19:46:13 +0100

    toggle results, add scss variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,46 @@
-const system = document.querySelector("#system").value;
-const convertBtn = document.querySelector('#convertBtn').addEventListener("click", convert);
+const convertBtn = document
+  .querySelector("#convertBtn")
+  .addEventListener("click", convert);
+
+const system = document.querySelector("#system");
+//Toggle Result Blocks
+system.addEventListener("change", function () {
+  Array.from(document.getElementsByClassName("results__block")).forEach(function (item) {
+    if (item.id == `${system.value}Block`) item.style.display = "none";
+    else item.style.display = "block";
+  });
+});
 
 function convert(e) {
-  let val = document.querySelector('#value').value;
-  var decResult = document.querySelector('#decResult');
-  var binResult = document.querySelector('#binResult');
-  var octResult = document.querySelector('#octResult');
-  var hexResult = document.querySelector('#hexResult');
+  let val = document.querySelector("#value").value;
+  var decResult = document.querySelector("#decResult");
+  var binResult = document.querySelector("#binResult");
+  var octResult = document.querySelector("#octResult");
+  var hexResult = document.querySelector("#hexResult");
 
-
-  if(val!=''){
+  if (val != "") {
     binResult.innerText = decToBin(val);
   }
   e.preventDefault();
-}
+};
+
+function decToOct(value) {
+  let n = value;
+  let octal = [];
+};
 
 function decToBin(value) {
   let n = value;
   let binary = [];
-  
-  for(let i=0; n>0; i++) {
-    binary[i] = Math.floor(n%2);
-    n=(Math.floor(n/2));
+
+  for (let i = 0; n > 0; i++) {
+    binary[i] = Math.floor(n % 2);
+    n = Math.floor(n / 2);
   }
-  binary=binary.reverse().map(String).join("");
+  binary = binary
+    .reverse()
+    .map(String)
+    .join("");
   console.log(`Binary: ${binary}`);
   return binary;
 }
-
-
