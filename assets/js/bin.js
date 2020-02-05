fc2cdc5b87a97b482062d07bb433ead874691378
unknown <mateusz.zahel@gmail.com>
2020-02-05 16:21:21 +0100

    add bin conversion

--- assets/js/bin.js
+++ assets/js/bin.js
@@ -1,19 +1,22 @@
 import {decResult, octResult, hexResult} from "./handlers";
 
 function binToDec(val) {
-  // var dec = 0;
-  // var nums = val.split("").map(Number);
-  // console.log(nums);
-  // for(let i=0; i<=(nums.length-1); i++) {
-  //   if(nums[i] === 1) {
-  //     console.log(dec)
-  //     dec+=Math.pow(2, nums.length-i-1)
-  //   }
-  // }
-  // console.log("dec = ", dec)
+  let n = val;
+  return parseInt(n, 2);
+}
+
+function binToOct(val) {
+  let n = binToDec(val);
+  return n.toString(8);
+}
+
+function binToHex(val) {
+  let n = binToDec(val);
+  return n.toString(16).toUpperCase();
 }
 
 export function convertFromBin() {
-  let val = document.getElementById("numVal").value;
-  binToDec(val);
+  let val = document.getElementById("numVal").value;  decResult.textContent = binToDec(val);
+  octResult.textContent = binToOct(val);
+  hexResult.textContent = binToHex(val);
 }
